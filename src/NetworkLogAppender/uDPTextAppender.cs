68c568a867b178c0ba9e937515cfd1de994eda44
Christia James <cjh@pentia.dk>
2017-01-06 13:37:11 +0100

    added config options

--- src/NetworkLogAppender/uDPTextAppender.cs
+++ src/NetworkLogAppender/uDPTextAppender.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using log4net.Appender;
 using log4net.Core;
@@ -7,29 +10,57 @@ namespace NetworkLogAppender
 {
     public class UdpTextAppender : AppenderSkeleton
     {
+
+        private Object localLock = new Object();
+        private UdpClient _udpClient;
+        private int portNumber;
+        private IPAddress _ipAddress;
+        private bool _isValidated = false;
+
+
+        public string Port { get; set; }
+
+        public string Destination { get; set; }
        
         public UdpTextAppender()
         {
-
+            
+            _udpClient = new UdpClient();
         }
 
         protected override void Append(LoggingEvent loggingEvent)
         {
-            //var udpClient = new TcpClient("localhost", 9080);
+            if (!_isValidated)
+                ValidateConfiguration();
 
-            //var writer = new StreamWriter(udpClient.GetStream());
-
-            //writer.Write("Custom tcp appender calling!");
-
-            //writer.Close();
-            //udpClient.Close();
-
+            var ipEndPoint = new IPEndPoint(_ipAddress, portNumber);
             var logMessage = RenderLoggingEvent(loggingEvent);
-
-            var udpClient = new UdpClient();
             var message = Encoding.ASCII.GetBytes(logMessage);
-            udpClient.Send(message, message.Length, "localhost", 8085);
-            udpClient.Close();
+            lock (localLock)
+            {
+                _udpClient.Send(message, message.Length, ipEndPoint);
+            }
+            
         }
+
+        private void ValidateConfiguration()
+        {
+            // Validate port
+            if (Port == null || !int.TryParse(Port, out portNumber))
+                throw new ArgumentException("Invalid port number for Udp appender");
+
+            // validate desination
+            var hostAddresses = Dns.GetHostAddresses(Destination);
+            if (hostAddresses == null || hostAddresses.Length < 1)
+                throw new ArgumentException("Invaild destination address for udp appender");
+
+            _ipAddress = hostAddresses.First(p => p.AddressFamily == AddressFamily.InterNetwork);
+        }
+
+        protected override void OnClose()
+        {
+            _udpClient.Close();
+        }
+
     }
 }
